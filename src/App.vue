1857cd6ebe525d9765b7bc0e5ef9af43f7ea21c3
Lyle-Lyle <xiongjinyue.lyle@gmail.com>
2023-06-04 21:39:09 -0400

    template of website

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+
+
+<template>
+  <NavBar />
+  <router-view></router-view>
+</template>
+
+
+<script>
+
+import NavBar from './components/NavBar.vue';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+
+
+export default {
+  components: {
+    NavBar,
+  }
+}
+
+</script>
+
+<style scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkIndexView.vue'
+import RecordIndexView from '../views/record/RecordIndexView.vue'
+import RankListIndexView from '../views/ranklist/RankListIndexView.vue'
+import UserBotIndexView from '../views/user/bots/UserBotIndexView.vue'
+import NotFound from '../views/error/NotFound.vue'
+
+// 从上往下匹配
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/",
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RankListIndexView,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound,
+  },
+  {
+    path: "/catchAll(.*)",
+    redirect: "/404/"
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes
+})
+
+
+export default router
+
+
